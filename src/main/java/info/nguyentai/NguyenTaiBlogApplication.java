0deef581852fd33116f6632a5990fb240d68ab0c
nguyentaijs <nguyentaijs@gmail.com>
2017-05-07 23:36:21 +0700

    New Spring Boot Project

---
+++ src/main/java/info/nguyentai/NguyenTaiBlogApplication.java
@@ -0,0 +1,12 @@
+package info.nguyentai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTaiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTaiBlogApplication.class, args);
+	}
+}
